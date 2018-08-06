0d20e52c1bbd94cde3cdd046360735052c7697c9
Merryl DMello <rolan86@gmail.com>
2018-08-06 11:49:01 +0000

    Add functions for machine learning

--- steppingstone/ml/modeller/modelling.py
+++ steppingstone/ml/modeller/modelling.py
@@ -11,4 +11,16 @@ class Modeller(object):
             )
 
     def add_weights(self):
-        pass
+        all_tags = self.get_tags()
+        weighted_tags = {}
+        for tag in all_tags:
+            entry = input("Enter weight between 0 - 5 for {} ".format(tag))
+            if not isinstance(entry, int):
+                done = raw_input("Are you done adding weights? y/n")
+                if done == 'y':
+                    break
+            if not weighted_tags.get(tag):
+                weighted_tags[tag] = entry
+        with open('../../../weights.json', 'w') as js:
+            data = js.write(json.dumps(weighted_tags))
+
