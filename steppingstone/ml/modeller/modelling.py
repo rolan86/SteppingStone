9e8e84921dc8ade53b5e28013a6c19147c94ee42
Merryl DMello <rolan86@gmail.com>
2018-08-07 03:50:58 +0000

    Add changes to ml storage functionality

--- steppingstone/ml/modeller/modelling.py
+++ steppingstone/ml/modeller/modelling.py
@@ -1,13 +1,13 @@
 import json
 
-#from steppingstone import conf
+from steppingstone import conf
 from steppingstone.ml.storage.database import Storage
 
 
 class Modeller(object):
 
     def get_tags(self):
-        with open('../../../quotes.json') as js:
+        with open('../data/quotes.json') as js:
             data = json.loads(js.read())
             return list(
                 set([item for sublist in data for item in sublist['tags']])
@@ -16,14 +16,26 @@ class Modeller(object):
     def add_weights(self):
         all_tags = self.get_tags()
         weighted_tags = {}
+        only_tags = {}
+        weighted_tags_list = []
         for tag in all_tags:
             entry = input("Enter weight between 0 - 5 for {} ".format(tag))
             if not isinstance(entry, int):
-                done = raw_input("Are you done adding weights? y/n")
+                done = raw_input("Are you done adding weights? y/n ")
                 if done == 'y':
                     break
-            if not weighted_tags.get(tag):
-                weighted_tags[tag] = entry
-        with open('../../../weights.json', 'w') as js:
-            data = js.write(json.dumps(weighted_tags))
+            if not only_tags.get(tag):
+                weighted_tags = {}
+                only_tags[tag] = entry
+                weighted_tags['tag'] = tag
+                weighted_tags['weight'] = entry
+                weighted_tags_list.append(weighted_tags)
+        with open('../data/weights.json', 'w') as js:
+            data = js.write(json.dumps(weighted_tags_list))
+        storage = Storage('tag-weights')
+        storage.put_all('weights', weighted_tags_list)
 
+    def get_weights(self, tag_dict):
+        storage = Storage('tag-weights')
+        result = storage.get_one('weights', tag_dict)
+        return result.get('weight')
--- steppingstone/ml/storage/database.py
+++ steppingstone/ml/storage/database.py
@@ -5,17 +5,19 @@ class Storage(object):
 
     def __init__(self, dbname):
         self.hostname = 'localhost'
-        #self.weights = 'tag-weights'
+        self.dbname = dbname
         self.port = '27017'
 
-    def createDB(self):
+    def put_all(self, coll, cdict_list):
         client = MongoClient('{}:{}'.format(self.hostname, self.port))
         db = client[self.dbname]
-        return db
-
-    def put_all(self, db, coll, cdict):
         coll = db.coll
-        return coll.insert_one(cdict)
+        return coll.insert_many(cdict_list)
 
-    def get_one(self, conn, sdict):
-        return conn.find_one(sdict)
+    def get_one(self, coll, sdict):
+        client = MongoClient('{}:{}'.format(self.hostname, self.port))
+        db = client[self.dbname]
+        coll = db.coll
+        result = coll.find_one(sdict)
+        return result
+
